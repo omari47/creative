a8bbd3af6f7e8fc365c648df5dea3ccc990fb181
omari47 <shanomari611@gmail.com>
2025-05-28 09:37:20 +0300

    Update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # bhgfjdkgiokgibtkd[pg[r
 # ughdbrmhjbmrdgveorgbbrgvmor
 # ythrjnmbtrhjbm tkhb,rgbjtr,hbk,y5t
+# htjbn,tbrh,bjrkbkgvokr,g
 class Category(models.Model):
     """Categories for portfolio items"""
     name = models.CharField(max_length=50)
